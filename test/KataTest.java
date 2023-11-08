22bd353a3bd1d1a04d8c39f6d0fe98a64895df4c
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-08 21:20:02 +0000

    Initial Commit

---
+++ test/KataTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class KataTest {
+
+    private static void doTest(String str, String expected) {
+        String actual = assertDoesNotThrow(() -> Kata.incrementString(str), "Solution thrown an unexpected exception for str=\"" + str + "\"\n\n");
+        assertEquals(expected, actual, "Incorrect answer for str=\"" + str + "\"\n\n");
+    }
+
+    @Test
+    public void incrementString() {
+        doTest("foo", "foo1");
+    }
+}
