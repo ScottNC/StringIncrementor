3496dc8fa8b68be8dd72ae60632fa566d120af92
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-08 23:22:36 +0000

    increments 9 to next digit correctly

--- src/Kata.java
+++ src/Kata.java
@@ -1,32 +1,28 @@
-import java.util.stream.IntStream;
-
 import static java.lang.Integer.parseInt;
 
 public class Kata {
 
     public static String incrementString(String str) {
-
-        int finalLetterIndex = IntStream.range(0, str.length())
-                .filter(i -> isNotNumberString(str.charAt(i)))
-                .reduce((first, second) -> second)
-                .orElse(-1) + 1;
-
-        if (finalLetterIndex != str.length()) {
-            String numStr = str.substring(finalLetterIndex);
-
-            int zeros = numStr.lastIndexOf('0') + 1;
-            int nines = str.length() - finalLetterIndex - zeros;
-
-            if (numStr.equals("0".repeat(zeros) + "9".repeat(nines)))
-                return str.substring(0, finalLetterIndex) +
-                        "0".repeat(Math.max(zeros - 1, 0)) + "1" + "0".repeat(nines);
-            else
-                return str.substring(0, str.length() - 1) +
-                        (parseInt(str.substring(str.length() - 1)) + 1);
-        } else return str + 1;
+        return replaceCharacter(str, str.length() - 1);
     }
 
-    public static boolean isNotNumberString (char c) {
-        return !String.valueOf(c).matches("-?\\d+(\\.\\d+)?");
+    public static String replaceCharacter (String str, int index) {
+        char c = str.charAt(index);
+
+        if (c == '9')
+            return replaceCharacter(str.substring(0, index) + "0" + str.substring(index + 1), index - 1);
+
+        if (isNumberString(c)) {
+            return str.substring(0, index) + (parseInt(String.valueOf(c)) + 1) + str.substring(index + 1);
+        }
+
+        if (str.length() - 1 == index)
+            return str + 1;
+
+        return str.substring(0, index + 1) + "1" + str.substring(index + 1);
+    }
+
+    public static boolean isNumberString (char c) {
+        return String.valueOf(c).matches("-?\\d+(\\.\\d+)?");
     }
 }
--- test/KataTest.java
+++ test/KataTest.java
@@ -16,11 +16,13 @@ public class KataTest {
         doTest("foo4", "foo5");
         doTest("foo99", "foo100");
         doTest("foo001", "foo002");
+        doTest("foo29", "foo30");
         doTest("foo0000999", "foo0001000");
     }
 
     @Test
     public void largeNumbers() {
         doTest("aaaaaaaaaaaaaa923676545678123456", "aaaaaaaaaaaaaa923676545678123457");
+        doTest("aaaaaaaaaaaaaa5637363837282728279", "aaaaaaaaaaaaaa5637363837282728280");
     }
 }
